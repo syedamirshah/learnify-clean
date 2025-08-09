de62ced05bdb3c6dbe57fc85180c0f000519c427
Syed Amir Shah <syedamirshah@email.com>
2025-08-09 14:12:36 +0500

    Use DATABASE_URL (Postgres) with sqlite fallback

--- backend/learnify/settings.py
+++ backend/learnify/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,12 +84,12 @@ WSGI_APPLICATION = 'learnify.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        env="DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
