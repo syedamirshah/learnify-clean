67ef948ed6a585982e5d04aa29ef4f1ce95a3afb
Syed Amir Shah <syedamirshah@email.com>
2025-10-20 15:33:22 +0500

    Add provider field (default easypay) to Payment model

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path("initiate/", views.initiate, name="initiate"),
 
     # --- Easypay transaction flow ---
-    # Use <int:pk> because Payment.pk is an integer (BigAutoField)
-    path("easypay/start/<int:pk>/", views.easypay_start, name="easypay_start"),
+    # Use <uuid:pk> because Payment.pk is a UUIDField
+    path("easypay/start/<uuid:pk>/", views.easypay_start, name="easypay_start"),
     path("easypay/token-handler/", views.easypay_token_handler, name="easypay_token_handler"),
     path("easypay/status-handler/", views.easypay_status_handler, name="easypay_status_handler"),
 
     # --- Read APIs for signed-in users ---
     path("mine/", views.my_payments, name="my_payments"),
-    path("<int:pk>/", views.payment_detail, name="payment_detail"),
+    path("<uuid:pk>/", views.payment_detail, name="payment_detail"),
 ]
